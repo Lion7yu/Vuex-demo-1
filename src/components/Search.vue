33d3261711a0ad67e5fc122d426e5971e4d71729
Lionyu <779625444@qq.com>
2021-06-19 20:00:50 +0800

    Composition API

---
+++ src/components/Search.vue
@@ -0,0 +1,23 @@
+<template>
+  <input type="text" :value="search" @input="onChange" />
+  <span>城市 :{{searchText}}</span>
+</template>
+
+<script>
+import { computed } from '@vue/runtime-core'
+import { useStore } from 'vuex'
+export default {
+  setup(){
+    let store = useStore()
+    const onChange = e =>{
+      store.commit('setSearch',e.target.value)
+      store.dispatch('getWeather',e.target.value)
+    }
+    return{
+      search: computed(()=>store.state.search),
+      serchText: computed(()=>store.getters.serchText),
+      onChange
+    }
+  }
+}
+</script>
---
+++ src/components/Weather.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="weather">
+    <h2>{{searchText}} 天气详情</h2>
+    <p>天气 {{weather.text}}</p>
+    <p>体感温度 {{weather.feels_like}}</p>
+    <p>风力 {{weather.wind_class}}</p>
+    <button @click="setDefault">深圳天气</button>
+  </div>
+</template>
+
+<script>
+import { computed } from '@vue/runtime-core'
+import { useStore } from 'vuex'
+export default {
+  setup(){
+    const store = useStore()
+    const setDefault = ()=>{
+      store.commit('setSearch','深圳')
+      store.dispatch('getWeather','深圳')
+    }
+    return{
+      searchText: computed(()=>store.getters.searchText),
+      weather:computed(()=>store.state.weather),
+      setDefault
+    }
+  }
+}
+</script>
